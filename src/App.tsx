729a059a4cd43d6127c75e7ac16b7aa85306e7ec
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-27 19:18:41 -0300

    Criada as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes  />
+  </BrowserRouter>
+  )
 
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+
 
 export default App;
