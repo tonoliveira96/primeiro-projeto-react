000b176a5c53ee7973b1db9038c496f2c598f122
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-28 07:29:25 -0300

    Lidando com erros

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import api from '../../services/api';
 
 import logoImg from '../../assets/logo-app.svg';
 
-import { Title, Form, Repository } from './styles';
+import { Title, Form, Repository, Error } from './styles';
 
 interface Repository {
   fullname: string;
@@ -17,27 +17,38 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepository] = useState<Repository[]>([]);
 
+  //FAZ A BUSCAR NA API
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o nome do autor/repositório');
+      return;
+    }
 
-    const response = await api.get<Repository>(`repos/${newRepo}`);
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
 
-    const repository = response.data;
+      const repository = response.data;
 
-    setRepository([...repositories, repository]);
-    setNewRepo('')
+      setRepository([...repositories, repository]);
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca do repositório');
+    }
   }
 
+  //inicia a renderizaçãoem tela
   return (
     <>
       <img src={logoImg} alt="github Explorer" />
       <Title>Explore repositórios no Github </Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
@@ -46,9 +57,11 @@ const Dashboard: React.FC = () => {
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repository>
-        {repositories.map(repository => (
-        <a key={repository.fullname} href="teste">
+        {repositories.map((repository) => (
+          <a key={repository.fullname} href="teste">
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -58,8 +71,8 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiArrowRight size={20} />
-          </a>)
-        )}
+          </a>
+        ))}
       </Repository>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,57 +1,72 @@
-import styled from 'styled-components';
-import {shade} from 'polished';
+import styled, {css} from 'styled-components';
+import { shade } from 'polished';
 
-export const Title = styled.h1 `
-    font-size: 48px;
-    color: #3a3a3a;
-    max-width: 450px;
-    line-height: 56px;
+interface FromProps{
+  hasError: boolean;
+}
 
-    margin-top: 60px;
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
 
-`
+  margin-top: 60px;
+`;
 
-export const Form= styled.form `
+export const Form = styled.form<FromProps>`
   margin-top: 40px;
   max-width: 700px;
 
-   display: flex;
+  display: flex;
 
-   input{
-     flex: 1;
-     height:70px;
-     padding: 0 24px;
-     border:0;
-     border-radius: 5px 0 0 5px;
-     color: #3a3a3a;
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
 
-     &::placeholder{
-       color: #a8a8b3;
-     }
-   }
+    /* Tratatita de erro no css */
+    ${(props)=> props.hasError && css`
+      border-color: #c53030;
+    `}
 
-   button{
-     width: 210px;
-     height:70px;
-     background: #04d361;
-     border-radius:0px 5px 5px 0px;
-     border:0;
-     color: #fff;
-     font-weight: bold;
-     transition: background-color 0.2s;
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
 
-     &:hover{
-       background: ${shade(0.2,'#04d361')}
-     }
-   }
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
 
-`
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
 
 export const Repository = styled.div`
   margin-top: 80px;
-  max-width:700px;
+  max-width: 700px;
 
-  a{
+  a {
     background: #fff;
     border-radius: 5px;
     width: 100%;
@@ -63,44 +78,42 @@ export const Repository = styled.div`
     align-items: center;
     transition: transform 0.2s;
 
-    &:hover{
-      transform: translateX(10px)
+    &:hover {
+      transform: translateX(10px);
     }
 
-    & + a{
+    & + a {
       margin-top: 16px;
     }
 
-    img{
+    img {
       width: 64px;
       height: 64px;
       border-radius: 50%;
     }
 
-    div{
+    div {
       margin-left: 0 16px;
-      flex:1;
+      flex: 1;
 
-      strong{
+      strong {
         font-size: 20px;
-        color: #3d3d4d
+        color: #3d3d4d;
       }
 
-      p{
-        font-size:18px;
+      p {
+        font-size: 18px;
         color: #a8a8b3;
         margin-top: 4px;
       }
     }
-    svg{
-        margin-left: auto;
-        color: #cdcdd6;
-        transition: color 0.2s
-      }
-      &hover{
-        color: ${shade(0.8,'#cdcdd6')}
-      }
-
+    svg {
+      margin-left: auto;
+      color: #cdcdd6;
+      transition: color 0.2s;
+    }
+    &hover {
+      color: ${shade(0.8, '#cdcdd6')};
+    }
   }
-
-`
+`;
