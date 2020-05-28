cbdbc4c11c7383bfbdea2e5f8102d70895b4fe7f
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-27 22:39:59 -0300

    Retornando dados da API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,49 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiArrowRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo-app.svg';
 
-
 import { Title, Form, Repository } from './styles';
 
-const Dashboard: React.FC = () =>{
-  return(
-  <>
-    <img src={logoImg} alt="github Explorer"/>
-    <Title>Explore repositórios no Github </Title>
-
-    <Form>
-      <input placeholder="Digite onome do repositório"/>
-      <button type="submit">Pesquisar</button>
-    </Form>
-
-    <Repository>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/43159625?s=460&u=08567372edcab56e9f803be46ed3adb47a4843f5" alt="Everton"/>
-        <div>
-          <strong>Everton Form</strong>
-          <p>
-            Piriri pororo
-          </p>
-        </div>
-        <FiArrowRight size={20} />
-      </a>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/43159625?s=460&u=08567372edcab56e9f803be46ed3adb47a4843f5" alt="Everton"/>
-        <div>
-          <strong>Everton Form</strong>
-          <p>
-            Piriri pororo
-          </p>
-        </div>
-        <FiArrowRight size={20} />
-      </a>
-    </Repository>
-
-  </>
-  )
-
+interface Repository {
+  fullname: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
 }
 
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepository] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepository([...repositories, repository]);
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="github Explorer" />
+      <Title>Explore repositórios no Github </Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite onome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repository>
+        {repositories.map(repository => (
+        <a key={repository.fullname} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.fullname}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiArrowRight size={20} />
+          </a>)
+        )}
+      </Repository>
+    </>
+  );
+};
 
 export default Dashboard;
