93da57c3c0f03ce2b33c94e9ed7fe6ed720a9834
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-29 18:57:43 -0300

    Mostra os detalhes de um repositório

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo-app.svg';
 
@@ -10,48 +11,84 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then((response) => {
+      setRepository(response.data)
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data)
+    });
+  }, [params.repository]);
 
   return (
     <>
       <Header>
         <img src={logoImg} alt="GitHub Explorer" />
-        <Link to="/dashboard">
+        <Link to="/">
           <FiArrowLeft size={16} />
           Voltar
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img src="https://avatars3.githubusercontent.com/u/69631" alt="" />
-          <div>
-            <strong>Repositório</strong>
-            <p>piriri pororo</p>
-          </div>
-        </header>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
 
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues Abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues Abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
 
       <Issues>
-        <Link
-          to={`/repositories`}
-        >
+        <Link to={`/repositories`}>
           <div>
             <strong>hihiihi</strong>
             <p>kkkkkkkkkkk</p>
